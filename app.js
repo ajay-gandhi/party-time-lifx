a11bd8bad31dcdecd21d06c0200f6f948613e8c7
Ajay Gandhi <aag255@cornell.edu>
2018-11-10 09:53:29 -0800

    Modularize

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const LifxClient = require("node-lifx").Client;
 
-const LIGHT_IP = "192.168.128.103";
-const BRIGHTNESS = parseInt(process.argv[2]) || 50;
+const DEFAULT_BRIGHTNESS = 50;
 const SATURATION = 100;
 const DURATION = 600;
 const COLORS = [0, 30, 60, 120, 180, 210, 240, 300];
 
-const client = new LifxClient();
+module.exports = (function () {
 
-client.init();
+  function PartyTime(lightIp, brightness) {
+    this.client = new LifxClient();
+    this.client.init();
+    this.client.on("light-new", (light) => {
+      if (light.address === lightIp) {
+        this.light = light;
+      }
+    });
 
-console.log("Starting...");
-client.on("light-new", (light) => {
-  if (light.address === LIGHT_IP) {
-    console.log("Found light");
+    this.brightness = brightness || DEFAULT_BRIGHTNESS;
+  }
 
+  PartyTime.prototype.start = function () {
     let prev = -1;
-    light.on();
+    this.light.on();
     setInterval(() => {
       let selection = Math.floor(Math.random() * COLORS.length);
       while (selection === prev) selection = Math.floor(Math.random() * COLORS.length);
       prev = selection;
-      light.color(COLORS[selection], SATURATION, BRIGHTNESS);
+      this.light.color(COLORS[selection], SATURATION, this.brightness);
     }, DURATION);
   }
-});
 
-process.on("exit", () => light.off());
+  PartyTime.prototype.stop = function () {
+    this.light.off();
+  }
+
+})();
